9e9432eac46c1a85ff2103eca3b6b70a4bcdff90
ssandd01 <160320840+ssandd01@users.noreply.github.com>
2024-02-18 14:49:18 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,5 +6,29 @@ window.onload = function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
 	});
-}
 
+	document.querySelectorAll(".info-btn").forEach(function(button) {
+		button.addEventListener("click", function() {
+			var hiddenContent = this.parentElement.querySelector(".hiddeninfo");
+			if (hiddenContent.style.display === "none") {
+				hiddenContent.style.display = "block";
+				this.textContent = "Hide";
+			} else {
+				hiddenContent.style.display = "none";
+				this.textContent = "Look Info";
+			}
+		});
+	
+		button.addEventListener("click", function() {
+			var hiddenContent = this.parentElement.querySelector(".hiddeninfo");
+			if (hiddenContent.style.maxHeight) {
+				hiddenContent.style.maxHeight = null;
+			} else {
+				hiddenContent.style.maxHeight = hiddenContent.scrollHeight + "px";
+			}
+		});
+	});
+	
+	
+}
+  
